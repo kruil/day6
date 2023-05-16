d28e650143e9a2a41689b51bc38beed3a174dade
Ilia Krupko <ikrupko@ourgapps.com>
2023-05-16 22:21:07 +0400

    Done

--- Day6/ViewController.swift
+++ Day6/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var blueBox: UIView = {
+        let view = UIView(frame: .init(origin: .zero, size: .init(width: 80, height: 80)))
+        view.backgroundColor = .systemBlue
+        view.layer.cornerRadius = 8
+        view.center = self.view.center
+        return view
+    }()
+    
+    lazy var dynamicAnimator = UIDynamicAnimator(referenceView: self.view)
+    lazy var snapBehavior = UISnapBehavior(item: blueBox, snapTo: view.center)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(onTap)))
+        view.addSubview(blueBox)
+        
+        dynamicAnimator.addBehavior(snapBehavior)
     }
-
-
+    
+    @objc func onTap(_ sender: UITapGestureRecognizer) {
+        snapBehavior.snapPoint = sender.location(in: nil)
+    }
 }
 
