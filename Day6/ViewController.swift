ff9a2d41dfc97410737fe4edb35cbc38ae9334c2
Ilia Krupko <ikrupko@ourgapps.com>
2023-05-16 21:55:00 +0400

    Initial Commit

---
+++ Day6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day6
+//
+//  Created by Ilya Krupko on 16.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
